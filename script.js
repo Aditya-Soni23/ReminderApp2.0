faa8c936b09249343dfc0eb30f43925426bb9bf9
Aditya-Soni23 <adityasonihyderabad@gmail.com>
2025-02-05 15:17:54 +0530

    nice

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-function showNotification() {
+document.getElementById("startBtn").addEventListener("click", function() {
+    // Request permission when user clicks the button
     if (Notification.permission === "granted") {
-        new Notification("Reminder", {
-            body: "Time to drink water",
-            icon: 'diamond.jpg'
-        });
+        startReminder();
     } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(permission => {
             if (permission === "granted") {
-                showNotification();
+                startReminder();
             }
         });
     }
+});
+
+function startReminder() {
+    // Convert minutes to milliseconds
+    let reminderTime = 5000;
+
+    // Show reminder notification
+    function showNotification() {
+        if (Notification.permission === "granted") {
+            new Notification("Reminder", {
+                body: "Time to drink water",
+                icon: 'diamond.jpg'
+            });
+        }
+    }
+
+    // Set the interval for reminders
+    setInterval(showNotification, reminderTime);
 }
-
-// Convert minutes to milliseconds
-let reminderTime = 5000;
-
-setInterval(showNotification, reminderTime);
