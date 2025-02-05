3df64bd11493c8df960e8fe70d5d2021e5d03195
Aditya-Soni23 <adityasonihyderabad@gmail.com>
2025-02-05 14:39:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function showNotification() {
+    if (Notification.permission === "granted") {
+        new Notification("Reminder", {
+            body: "Time to drink water",
+            icon: 'diamond.jpg'
+        });
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                showNotification();
+            }
+        });
+    }
+}
+
+// Convert minutes to milliseconds
+let reminderTime = 50000;
+
+setInterval(showNotification, reminderTime);
